5d447e640ae6aca079617ba7039b192c23aa3293
YuriyDub <105994926+YuriyDub@users.noreply.github.com>
2023-05-26 16:25:27 +0300

    fixed images

--- src/components/PlaceHolder/index.js
+++ src/components/PlaceHolder/index.js
@@ -5,7 +5,7 @@ function PlaceHolder() {
   return (
     <div className={styles.loader}>
       <h2>Loading</h2>
-      <img src="/img/icons/loading.png" alt="LoadImage" />
+      <img src="./img/icons/loading.png" alt="LoadImage" />
     </div>
   );
 }
